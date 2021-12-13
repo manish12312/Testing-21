a0727a16512522e5476c8dcec12a04f0ede39b20
MANISH <MANISH@DESKTOP-NPV50CU>
2021-12-13 15:39:04 +0530

    Done by Amit

---
+++ GitDemo-301/src/A.java
@@ -0,0 +1,31 @@
+
+public class A 
+{
+
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+	
+		A a1=new A();
+		a1.m1();
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+	private void m1() 
+	{
+		
+		System.out.println(" M1 of A");
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+	
+}
